973ded2dd068a88c10d1737e1a6c1fae0b1e650a
Alejandro <acuellarh@gmail.com>
2020-03-28 10:44:54 -0500

    Repeater feature added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      repeat: ""
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={this.updateRepeater.bind(this)} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.repeat}</p>
       </div>
     );
   }
+
+  updateRepeater(event){
+    this.setState({
+      repeat: event.target.value
+    });
+  }
+
 }
 
 export default App;
